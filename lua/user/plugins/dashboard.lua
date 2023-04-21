aeae36b58f27cee274bb75939f42b33f44a62222
Keven Goncalves <keven.jm.goncalves@gmail.com>
2023-04-21 10:07:16 +0200

    feat(persistance):add restore last session to dashboard

--- lua/user/plugins/dashboard.lua
+++ lua/user/plugins/dashboard.lua
@@ -1,3 +1,19 @@
 lvim.builtin.alpha.active = true
 lvim.builtin.alpha.mode = "dashboard"
 lvim.builtin.alpha.dashboard.section.footer.val = {}
+lvim.builtin.alpha.dashboard.section.buttons.val[5] = {
+  opts = {
+    align_shortcut = "right",
+    cursor = 5,
+    hl_shortcut = "Include",
+    keymap = {
+      "n", "l", "<CMD>:lua require('persistence').load({ last = true })<CR>",
+      { noremap = true, nowait = true, silent = true }
+    },
+    position = "center",
+    shortcut = "l",
+    width = 50
+  },
+  type = "button",
+  val = "  Restore Last Session"
+}
